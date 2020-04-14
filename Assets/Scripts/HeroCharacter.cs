7a80a4860f97e33c96d60820a018639bbf43cbbc
KameriiJ <jenny.sirarat@gmail.com>
2020-04-14 19:44:09 +0700

    first commit

---
+++ Assets/Scripts/HeroCharacter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class HeroCharacter : MonoBehaviour
+{
+
+    public GameObject target;
+
+    private NavMeshAgent agent;
+    private Animator anim;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim.SetTrigger("Walk_Anim");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("anim_Walk_Loop"))
+        {
+            agent.destination = target.transform.position;
+        }
+    }
+}
