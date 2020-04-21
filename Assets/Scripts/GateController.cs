8c7bef48936cf8c6a31bf40222527f27b3201e0a
KameriiJ <jenny.sirarat@gmail.com>
2020-04-21 16:06:28 +0700

    added door

---
+++ Assets/Scripts/GateController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateController : MonoBehaviour
+{
+    public GameObject canvas;
+    public GameObject target;
+
+    Animator anim;
+
+    private void Awake()
+    {
+        anim = target.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && canvas.activeSelf) anim.SetTrigger("activeGate");    
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        canvas.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        canvas.SetActive(false);
+    }
+}
